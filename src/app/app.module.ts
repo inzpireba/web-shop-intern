d732b4d9e004b797801b6c3d145037b77d5eebe7
amina2612 <amina.tarabar2018@size.ba>
2021-04-11 13:47:58 +0200

    registration component angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/user.model.ts
@@ -0,0 +1,7 @@
+export class User {
+    email: string = '';
+    password: string = '';
+    password2: string ='';
+    firstName: string = '';
+    lastName: string = '';
+}
