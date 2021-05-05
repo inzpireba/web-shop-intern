7a39ff323530abc35b9f48e47e34809e104df00e
Alem <alem.hodzic1999@gmail.com>
2021-05-05 21:57:00 +0200

    contact form functionality with emailJS

--- src/app/components/contact-page/contact-page.component.ts
+++ src/app/components/contact-page/contact-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
@@ -10,6 +10,16 @@ export class ContactPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  // npm install emailjs-com
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('service_ryj7eqq', 'template_9gtqai6', e.target as HTMLFormElement, 'user_gCiOA3ejIvKtil3O0bGRt')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
   }
 
 }
